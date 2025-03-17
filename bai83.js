33db6ad849f13c53718edf018502c03102517812
thien <nhatthienn911@gmail.com>
2025-03-17 11:38:26 +0700

    bai

---
+++ bai83.js
@@ -0,0 +1,13 @@
+
+var date = 2;
+if (date == 2) {
+    console.log('Hom nay la thu 2');
+}else
+if (date == 3) {
+    console.log('Hom nay la thu ');
+}else
+if (date == 4) {
+    console.log('Hom nay la thu 4');
+}else{
+    console.log('Khong biet')
+}
