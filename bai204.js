659214af2233b61623e283e14d84c108492ad063
thien <nhatthienn911@gmail.com>
2025-03-21 11:43:36 +0700

    204

---
+++ bai204.js
@@ -0,0 +1,11 @@
+if (!String.prototype.includes){
+    String.prototype.includes = function(){
+        'use strict';
+        if(search instanceof RegExp){
+            throw TypeError('firit argument must not be a RegExp')
+        }
+    }
+        if(s)
+
+    }
+     
